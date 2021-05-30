84dd4786fa4a287f5e5f04f1ac35bb614f219ad1
Mandeep <mandeep.kaur@blockfi.com>
2021-05-30 14:21:54 +0530

    Dynamic values: V1.0

--- cypress/integration/CucumberTests/login.feature
+++ cypress/integration/CucumberTests/login.feature
@@ -5,6 +5,7 @@ Login Test Cases
 Scenario: Login with valid credentials
 
 Given Login to the application
-When Enter valid Credentials 
+When I type username as "username"
+And I type password as "password"
 Then click Login
 And See the homepage
--- cypress/integration/CucumberTests/login/loginSteps.js
+++ cypress/integration/CucumberTests/login/loginSteps.js
@@ -2,18 +2,17 @@ import { Given , And , Then , When} from "cypress-cucumber-preprocessor/steps";
 
 Given('Login to the application', () => {
    cy.visit('http://zero.webappsecurity.com/login.html')
-  
   })
   
-  When('Enter valid Credentials', () => {
-    cy.get('#user_login').type('username')
-    cy.get('#user_password').type('password')
+  When('I type username as {string}',username=> {
+    cy.get('#user_login').type(username)
   })
-  
+  And('I type password as {string}',password=> {
+    cy.get('#user_password').type(password)
+  })
   Then('click Login', () => {
-    cy.get('input[name="submit"]').type('username')
+    cy.get('input[name="submit"]').click()
   })
-  
   And('See the homepage', () => {
     cy.get('a:contains("Zero Bank")').should('be.visible')
   })
